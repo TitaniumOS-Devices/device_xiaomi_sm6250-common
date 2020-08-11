b9ff7835c65f737a238deee6becf14ce8b679e63
kazerine <kamikaze.mrinal@gmail.com>
2020-08-12 00:18:36 +0530

    sm6250-common: Get rid of megvii dependencies in camera.qcom.so
    Change-Id: I97058c320d06819a468ebe38e2704360e9aa91db
    Signed-off-by: Arian <arian.kulmer@web.de>
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- common.mk
+++ common.mk
@@ -111,6 +111,9 @@ PRODUCT_PACKAGES += \
     Snap
 
 PRODUCT_PACKAGES += \
+    libshim_megvii
+
+PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64
 
