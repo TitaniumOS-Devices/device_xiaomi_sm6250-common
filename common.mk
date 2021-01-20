2cb65c8865fd3b20c9bee0bf1d7f28106a581cd0
SonalSingh18 <sonal.singh.19993@gmail.com>
2021-01-20 14:00:49 +0530

    sm6250-common: use prebuilt sensorservice from vndk v29
    * HidlServiceManagement: getService: Trying again for android.frameworks.sensorservice@1.0::ISensorManager
    
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- common.mk
+++ common.mk
@@ -391,6 +391,8 @@ PRODUCT_PACKAGES += \
     libprotobuf-cpp-lite-vendorcompat
 
 PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm/arch-arm-armv7-a-neon/shared/vndk-core/android.frameworks.sensorservice@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib/android.frameworks.sensorservice@1.0.so \ 
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/android.frameworks.sensorservice@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/android.frameworks.sensorservice@1.0.so \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libhidlcache.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlcache.so
 
 # Vulkan
